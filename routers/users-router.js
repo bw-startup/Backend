ef6337bb1e04c2176a9b1727c615e8a1b921355e
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-16 00:22:38 +0100

    Move Auth Endpoints Into Separate File

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,62 +1,10 @@
 const router = require("express").Router();
-const { register, login } = require("../helpers/users-helpers.js");
 
-// Register or CREATE- takes in username, email, password, returns object with new user
-router.post("/register", async (req, res) => {
-  let user = req.body;
-  if (!user.email || !user.password) {
-    res.status(401).json({
-      message: "Email and password are required for registration"
-    });
-  }
-  try {
-    await register(user);
-    res.status(200).json({
-      message: `Successfully created user ${user.email}`
-    });
-  } catch (error) {
-    if (error === 500) {
-      res.status(500).json({ message: "Error registering user" });
-    }
-    else if (error === 406) {
-      res.status(406).json({
-        message: "Sorry, the email already exists. Please use a different email."
-      });
-    }
-  }
-});
+// TODO: [PUT] /users/:id (edit user details)
 
-//LOGIN- auth required, takes in username and password, returns message, username, and token
-//To do: Create token, add bcrypt, and return token along with message.
-router.post("/login", async (req, res) => {
-  let { email, password } = req.body;
+// TODO: [GET] /me (get current user object)
 
-  if (email && password) {
-    try {
-      const token = await login({ email, password });
-      res.status(200).json({
-          message: `Welcome ${email}`,
-          token
-      });
-    } catch (error) {
-      if (error === 500) {
-        res.status(500).json({
-          message: "Server failed to retrieve users"
-        });
-      }
-      else if (error === 406) {
-        res.status(406).json({
-          message: "Invalid credentials. Please try again."
-        });
-      }
-    }
-
-  } else {
-    res.status(401).json({ message: "Please provide username and password" });
-  }
-});
-
-//GET list of all users - auth required
+// GET list of all users - auth required
 router.get("/users", async (req, res) => {
   try {
     const users = await Users.find();
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const server = express();
 
 const usersRouter = require("./routers/users-router.js");
+const authRouter = require("./routers/auth-router.js");
 
 server.use(express.json());
 
+server.use("/api/auth", authRouter);
 server.use("/api", usersRouter);
 
 server.get("/", (req, res) => {
